4fd88a01243941b126bee3b47ce63c715636308e
Kattsyn <vov4ikkat3@mail.ru>
2024-09-25 14:12:52 +0300

    init commit

---
+++ src/main/java/kattsyn/dev/ApartmentsUnderConstruction/ApartmentsUnderConstructionApplication.java
@@ -0,0 +1,13 @@
+package kattsyn.dev.ApartmentsUnderConstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsUnderConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsUnderConstructionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kattsyn/dev/ApartmentsUnderConstruction/ApartmentsUnderConstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package kattsyn.dev.ApartmentsUnderConstruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsUnderConstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
