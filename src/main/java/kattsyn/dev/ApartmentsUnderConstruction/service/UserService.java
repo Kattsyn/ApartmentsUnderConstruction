3b0527a139f6148cc82e0a82b7ed3e0e0a05a1b0
Kattsyn <warshard1337@gmail.com>
2024-11-28 17:30:59 +0300

    added AuthController.java added login page overrided loadByUsername in UserService.java

--- src/main/java/kattsyn/dev/ApartmentsUnderConstruction/service/UserService.java
+++ src/main/java/kattsyn/dev/ApartmentsUnderConstruction/service/UserService.java
@@ -1,11 +1,15 @@
 package kattsyn.dev.ApartmentsUnderConstruction.service;
 
+import kattsyn.dev.ApartmentsUnderConstruction.entities.User;
 import kattsyn.dev.ApartmentsUnderConstruction.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +18,21 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException(String.format("User %s not found", username)));
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = findByUsername(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList())
+        );
     }
+
+
 }
